0bb68b877ca49b07accc92724558535b03d25395
AntonioChicano <antonio.chicano@innovadeluxe.com>
2021-09-22 14:59:55 +0200

    Guardado

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,22 +12,31 @@ export class PorPaisComponent {
 
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) { }
 
-  buscar() {
+  buscar(termino: string) {
     this.hayError = false;
-    console.log(this.termino);
+    this.termino = termino;
 
-    this.paisService.buscarPais(this.termino)
+
+    this.paisService.buscarPais(termino)
       .subscribe((paises) => {
         console.log(paises);
+        this.paises = paises;
 
 
       }, (err) => {
         this.hayError = true;
+        this.paises = [];
       });
 
+  }
+
+  sugerencias(termino: string) {
+    this.hayError = false;
+    // TO DO: crear sugerencias 
   }
 
 }
