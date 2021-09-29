142f970e916370833acda345f39e86eb9fecf7c2
AntonioChicano <antonio.chicano@innovadeluxe.com>
2021-09-29 21:12:25 +0200

    Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [
     AppComponent
   ],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/PaisesApp-Angular/' }],
+  providers: [{ provide: APP_BASE_HREF, useValue: 'PaisesApp-Angular' }],
   imports: [
     BrowserModule,
     AppRoutingModule,
